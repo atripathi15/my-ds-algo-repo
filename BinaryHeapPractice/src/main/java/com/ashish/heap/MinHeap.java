ae8d1bc1232f343ffcd427e3885de4b97e823a1d
atripathi15 <er.ashishtripathi@gmail.com>
2022-07-26 22:43:52 +0530

    Added BinaryHeap project

--- BinaryHeapPractice/src/main/java/com/ashish/heap/MinHeap.java
+++ BinaryHeapPractice/src/main/java/com/ashish/heap/MinHeap.java
@@ -1,10 +1,13 @@
 package com.ashish.heap;
 
+import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Component
 public class MinHeap {
 	
 	private int[] arr;
@@ -26,31 +29,98 @@ public class MinHeap {
 	}
 	
 	public int parent(int i) {
-		return (int) Math.floor((double)i-1/2);
+		return (int) Math.floor((double)(i-1)/2);
 	}
 	
-	public void insert(int x){
-		
+	/**
+	 *  Time Complexity : O(n) , Space Complexity : O(1)
+	*/
+	public void insert(int x) {
+		if (size == capacity)
+			return;
+		size++;
+		arr[size - 1] = x;
+		for (int i = size - 1; i > 0 && arr[parent(i)] > arr[i];) {
+			swap(i, parent(i));
+			i = parent(i);
+		}
 	}
 	
+	/**
+	 *  Time Complexity : O(logn) , Space Complexity : O(logn)
+	*/
 	public void minHeapify(int index) {
-		
+		int left = left(index);
+		int right = right(index);
+		int smallest = index;
+		if (left < size && arr[smallest] > arr[left]) {
+			smallest = left;
+		}
+		if (right < size && arr[smallest] > arr[right]) {
+			smallest = right;
+		}
+		if (smallest != index) {
+			swap(index, smallest);
+			minHeapify(smallest);
+		}
 	}
 	
+	private void swap(int from, int to) {
+		int temp = arr[from];
+		arr[from] = arr[to];
+		arr[to] = temp;
+	}
+
+	/**
+	 *  Time Complexity : O(logn) , Space Complexity : O(logn)
+	*/
 	public int extractMin() {
-		return 1;
+		if(size == 0) {
+			return Integer.MIN_VALUE;
+		}
+		if(size == 1) {
+			size--;
+			return arr[0];
+		}
+		swap(0, size-1);
+		size--;
+		minHeapify(0);
+		return arr[size];
 	}
 	
+	/**
+	 *  Time Complexity : O(logn) , Space Complexity : O(1)
+	*/
 	public void decreaseKey(int index, int x) {
-		
+		if (index < size) {
+			arr[index] = x;
+			while (index != 0 && arr[parent(index)] > arr[index]) {
+				swap(index, parent(index));
+				index = parent(index);
+			}
+		}
 	}
 	
+	/**
+	 *  Time Complexity : O(logn)
+	*/
 	public void delete(int i) {
-		
+		if (i < size) {
+			decreaseKey(i, Integer.MIN_VALUE);
+			extractMin();		
+		}
+
 	}
 	
-	public void buildHeap() {
-		
+	/**
+	 * Given a random array, rearrange its elements to form a min heap
+	 * Time Complexity : O(nlogn)
+	 */
+	public void buildMinHeap(int[] a) {
+		int size = a.length;
+		for(int i = size-2/2;i>=0;i--) {
+			minHeapify(i);
+		}
 	}	
 
 }
