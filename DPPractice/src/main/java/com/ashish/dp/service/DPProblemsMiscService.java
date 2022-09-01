9a2ea558231689e81468e324f20fefcdb0e82cad
atripathi15 <er.ashishtripathi@gmail.com>
2022-09-01 11:40:21 +0530

    commit

--- DPPractice/src/main/java/com/ashish/dp/service/DPProblemsMiscService.java
+++ DPPractice/src/main/java/com/ashish/dp/service/DPProblemsMiscService.java
@@ -25,5 +25,68 @@ public class DPProblemsMiscService {
 		return result;
 	}
 	
+	public int getMaxCuts(int n, int a, int b, int c) {
+		int[] dp = new int[n + 1];
+		dp[0] = 0;
+		for (int i = 1; i <= n; i++) {
+			dp[i] = -1;
+			if (i - a >= 0)
+				dp[i] = Math.max(dp[i], dp[i - a]);
+			if (i - b >= 0)
+				dp[i] = Math.max(dp[i], dp[i - b]);
+			if (i - c >= 0)
+				dp[i] = Math.max(dp[i], dp[i - c]);
+
+			if (dp[i] != -1) {
+				dp[i]++;
+			}
+		}
+		return dp[n];
+	}
+	
+	/**
+	 * Get minimum number of coins required to reach a given value
+	 */
+	public int getMinCoinsDP(int[] coins, int n, int value) {
+		int[] dp = new int[value + 1];
+		dp[0] = 0;
+		for (int i = 1; i <= value; i++) {
+			dp[i] = Integer.MAX_VALUE;
+		}
+		for (int i = 1; i <= value; i++) {
+
+			for (int j = 0; j < n; j++) {
+				if (coins[j] <= i) {
+					int sub_res = dp[i - coins[j]];
+					dp[i] = Math.min(dp[i], sub_res + 1);
+				}
+			}
+		}
+		return dp[value];
+	}
+	
+	/**
+	 * Time Complexity O(f2*e)
+	 * Space Complexity O(f*e)
+	 * @param f : no of floors
+	 * @param e : no of eggs
+	 * @return minimum number of egg drops required to find the egg breaking floor
+	 */
+	public int getMinEggDropCountDP(int f, int e) {
+		int[][] dp = new int[f+1][e+1];
+		for(int i=1;i<=e;i++) {
+			dp[0][i]=0;
+			dp[1][i]=1;
+		}
+		for(int j=1;j<=f;j++) dp[j][1]=j;
+		for(int i=2;i<=f;i++)
+			for(int j=2;j<=e;j++) {
+				dp[i][j] = Integer.MAX_VALUE;
+				for(int x=1;x<=i;x++)
+				dp[i][j] = Math.min(dp[i][j],1+Math.max(dp[x-1][j-1], dp[i-x][j]));
+			}
+		return dp[f][e];		
+	}
+	
 
 }
--- DPPractice/src/test/java/com/ashish/dp/DPProblemsMiscServiceTest.java
+++ DPPractice/src/test/java/com/ashish/dp/DPProblemsMiscServiceTest.java
@@ -28,5 +28,40 @@ public class DPProblemsMiscServiceTest {
 		assertEquals(4, result);
 
 	}
+	
+	@Test
+	public void maxCutTest() {
+		int result = dpProblemsMiscService.getMaxCuts(5,1,2,3);
+		assertEquals(5, result);
+		
+		result = dpProblemsMiscService.getMaxCuts(23,12,11,13);
+		assertEquals(2, result);
+		
+		result = dpProblemsMiscService.getMaxCuts(3,2,4,2);
+		assertEquals(-1, result);
+	}
+	
+	@Test
+	public void getMinCoinsTest() {
+		
+		int result = dpProblemsMiscService.getMinCoinsDP(new int[]{3,4,1}, 3, 5);
+		assertEquals(2, result);
+		
+		result = dpProblemsMiscService.getMinCoinsDP(new int[]{25,10,5}, 3, 30);
+		assertEquals(2, result);
+		
+		result = dpProblemsMiscService.getMinCoinsDP(new int[]{9,6,5,1}, 4, 11);
+		assertEquals(2, result);
+		
+	}
+	
+	@Test
+	public void getMinEggDropCountTest() {
+		int result = dpProblemsMiscService.getMinEggDropCountDP(3, 2);
+		assertEquals(2, result);
+		
+		result = dpProblemsMiscService.getMinEggDropCountDP(10, 2);
+		assertEquals(4, result);
+	}
 
 }
