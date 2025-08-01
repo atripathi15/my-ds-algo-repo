772b8a0f9bcd9da5fe57f0c80bc4ae44939f7baf
atripathi15 <er.ashishtripathi@gmail.com>
2025-08-02 00:34:06 +0530

    LinkedList Practice Project

---
+++ src/test/java/com/ashish/ll/service/ReverseLinkedListServiceTest.java
@@ -0,0 +1,30 @@
+package com.ashish.ll.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+import com.ashish.ll.model.Node;
+
+public class ReverseLinkedListServiceTest {
+	
+	@Test
+    public void testReverseLinkedList_MultipleNodes() {
+        // 1 -> 2 -> 3 -> null
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        ReverseLinkedListService service = new ReverseLinkedListService();
+        Node reversed = service.reverseLinkedList(head);
+
+        // Expect: 3 -> 2 -> 1 -> null
+        assertEquals(3, reversed.data);
+        assertEquals(2, reversed.next.data);
+        assertEquals(1, reversed.next.next.data);
+        assertNull(reversed.next.next.next);
+    }
+
+
+}
