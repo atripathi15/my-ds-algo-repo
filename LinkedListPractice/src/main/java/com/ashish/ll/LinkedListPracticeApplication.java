3631348cde2781e9d759be31030dfc08428cb5ac
atripathi15 <er.ashishtripathi@gmail.com>
2025-08-02 10:33:41 +0530

    first commit

---
+++ LinkedListPractice/src/main/java/com/ashish/ll/LinkedListPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.ll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedListPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedListPracticeApplication.class, args);
+	}
+
+}
---
+++ LinkedListPractice/src/test/java/com/ashish/ll/LinkedListPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.ll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedListPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
